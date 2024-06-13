ea8a8d02056ce1ac16a2f917e91db1a41018db60
Abhishek Bhatt <abhishekbhatti625@gmail.com>
2024-06-13 15:29:11 +0545

    Arithmetiic Opeators

---
+++ index.js
@@ -0,0 +1,18 @@
+// arithmetic operators = operands(values, variables, etc)
+//                        operators(+ - * /)
+//                        ex. 11 = x +5;
+
+
+let students = 30;
+
+students = students + 1
+students = students - 1
+students = students * 1
+students = students / 1
+students = students ** 2
+students = students % 4
+students += 2;
+
+let result = 1 + 2 * 3 + 4 ** 2;
+
+console.log(result);
